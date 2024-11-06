0b796cf4bd5ea0ea1c0573e4301f4b4d6a841f86
ellagself <eself6@ut.utm.edu>
2024-11-06 11:54:32 -0600

    functions

--- library.cpp
+++ library.cpp
@@ -38,3 +38,78 @@ Library::destructHleper(Entry *curr){
 
   delete curr; 
 }
+
+void Library::insert_sorted(string author, string title){
+  Entry* newEntry = new Entry;
+  newEntry->author = authore;
+  newEntry->title = title; 
+  newEntry->next = NULL; 
+
+  // if list is empty insert here
+  if(head == NULL){
+    newEntry-> next = head;
+    head = newEntry;
+  }
+  else {
+    Entry* curr = head;
+    while( curr->next != NULL && curr->next->title < title){
+      curr = curr->next;
+    }
+    newEntry->next = curr->next;
+    curr->next = newEntry; 
+  }
+}
+
+//lookup but its find_author
+string Library::find_author(string title){
+    Entry* curr = head;
+    while (curr != NULL){
+      if (curr->author == author){
+	return curr->title; 
+      }
+      curr = curr->next;
+    }
+    return ""; 
+  }
+
+
+// reverse lookup, find books by certain author
+string Library::find_album(string title){
+   Entry* curr = head;
+   while (curr != NULL){
+     if (curr->title == title){
+       return curr->author; // how do i get it to return all of them
+     }
+     curr = curr->next; 
+   }
+   return "";
+ }
+
+
+void Library::delete(string author, string title){
+  Entry *temp;
+  Entry* eraser = NULL;
+
+  // case 1, empty list
+  if (head == NULL){
+    return;
+    // nothing to delete, just returns
+  }
+  // case 2, delete the head of the list
+  if(head->author == author) {
+    eraser = head;
+    head = head->next;
+    delete eraser;
+    return; 
+  }
+
+  while(temp->next != NULL) {
+    if(temp->next->author == author){
+      eraser = temp->next;
+      temp->next = eraser->next;
+      delete eraser;
+      return;
+    }
+    temp = temp->next;
+  }
+}
