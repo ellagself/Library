a9734739c3873745ca35110aa8a38441593f69ea
cameronrtucker <ctucke31@ut.utm.edu>
2024-11-07 15:10:20 -0600

    added constructor

--- book.h
+++ book.h
@@ -1,9 +1,15 @@
-// book class or struct
+#ifndef BOOK_H
+#define BOOK_H
 
+// book class or struct
+#include <string>
+using namespace std;
 
 struct Book{
-  Entry *next;
   string author, title, isbn;
   int pages, year;
   float price;
+  Book(const string& author = "", const string& title = "", const string& isbn = "", int pages = 0, int year = 0, float price =0.0f)
+    : author (author), title(title), isbn(isbn), pages(pages), year(year), price(price) {} //defines constructor for book with default initial values
 }; 
+#endif
